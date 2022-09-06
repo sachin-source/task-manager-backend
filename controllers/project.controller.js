9dbf37d3baf09ff94feef20338e3fd84f65d74fe
sachin-source <sachinmgvt@gmail.com>
2022-09-06 21:26:05 +0530

    tagging the existing payment complete

--- controllers/project.controller.js
+++ controllers/project.controller.js
@@ -1,5 +1,4 @@
 const project = require('../models/projects');
-const payment = require('../models/payment');
 
 const addProject = (req, res) => {
     const { name } = req.body;
@@ -13,11 +12,5 @@ const listProjects = (req, res) => {
         return res.send({ status : !err, projects, message : err ? "error listing the projects, please try again later" : "list of all the projects" })
     })
 }
-
-payment.updateMany({}, {$set : { projectId : "63176cb78d43be54ae1c1b15"}}, (err, updateFlag) => {
-    console.log(err, updateFlag)
-})
-
-
 
 module.exports = { addProject, listProjects };
